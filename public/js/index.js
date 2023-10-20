15ebd2f59252970eb280119f3538baf65e72a9f8
RBalajisrinath <11407balaji@gmail.com>
2023-10-20 14:42:38 +0530

    ChatForums

--- public/js/index.js
+++ public/js/index.js
@@ -5,14 +5,3 @@ joinContainer.style.display = 'block';
 setTimeout(function(){
     joinContainer.style.opacity = 100;
 }, 500)
-
-// loginButton.addEventListener('click', function (e) {
-//     // joinContainer.style.display = 'none';
-//     e.preventDefault();
-//     console.log(e);
-//     joinContainer.style.opacity = 0;
-//     setTimeout(function(){
-//         return window.location.href = "chat.html";
-//     }, 1000)
-// })
-
--- server.js
+++ server.js
@@ -2,26 +2,51 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const formatMessage = require('./utils/messages')
-const {userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
+const mongoose = require('mongoose'); 
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/chatapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const messageSchema = new mongoose.Schema({
+    room: String,
+    username: String,
+    text: String,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 const admin = 'Admin';
-
 // Run when a client connects
-io.on('connection', socket => {
-    socket.on('joinRoom', ({ username, room }) => {
-        const user = userJoin(socket.id,username, room);
+io.on('connection', (socket) => {
+    socket.on('joinRoom', async ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
         socket.join(user.room);
-
-        // Welcome current user
-        socket.emit('message', formatMessage(admin, 'Welcome to Chat App!'));
+        // Fetch and send old messages for the room
+        try {
+            const messages = await Message.find({ room }).sort({ createdAt: 'asc' });
+            messages.forEach((msg) => {
+                socket.emit('message', formatMessage(msg.username, msg.text));
+            });
+        } catch (error) {
+            console.error(error);
+        }
 
         // Broadcast when a user connects
         socket.broadcast.to(user.room).emit('message', formatMessage(admin, `${user.username} has joined the chat`));
@@ -29,15 +54,26 @@ io.on('connection', socket => {
         // Send users and room info
         io.to(user.room).emit('roomUsers', {
             room: user.room,
-            users: getRoomUsers(user.room)
+            users: getRoomUsers(user.room),
         });
     });
 
     // Listen for chatMessage
-    socket.on('chatMessage', msg => {
+    socket.on('chatMessage', async (msg) => {
         const user = getCurrentUser(socket.id);
 
-        io.to(user.room).emit('message', formatMessage(user.username, msg));
+        const newMessage = new Message({
+            room: user.room,
+            username: user.username,
+            text: msg,
+        });
+
+        try {
+            await newMessage.save();
+            io.to(user.room).emit('message', formatMessage(user.username, msg));
+        } catch (error) {
+            console.error(error);
+        }
     });
 
     // Runs when client disconnects
@@ -51,11 +87,9 @@ io.on('connection', socket => {
         // Send users and room info
         io.emit('roomUsers', {
             room: user.room,
-            users: getRoomUsers(user.room)
+            users: getRoomUsers(user.room),
         });
     });
-})
-
-const PORT = 3000 || process.env.PORT;
-
+});
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
